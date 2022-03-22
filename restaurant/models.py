51ebb6558f16f6c57672f7019af039dba2eee7e5
RicardoIT-Web <rick_piedade@hotmail.com>
2022-03-22 18:15:13 +0000

    create table model to add tables to restaurant and booking model to add bookings

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Available"), (1, "Unavailable"))
+
+
+class Table(models.Model):
+    """
+    All tables deemed to be single table seating 2 people each.
+    """
+    TABLE_LOCATION = (
+        ('OUTSIDE', 'OUTSIDE'),
+        ('WINDOW', 'WINDOW'),
+        ('HALL', 'HALL')
+    )
+    number = models.IntegerField()
+    location = models.CharField(max_length=10, choices=TABLE_LOCATION)
+    capacity = models.IntegerField()
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return f'Table {self.number}, {self.location}. Capacity {self.capacity} guests'
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    group_size = models.PositiveIntegerField()
+    date = models.DateField(blank=True)
+    start_time = models.TimeField(blank=True)
+    end_time = models.TimeField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.table}. Booked by {self.user} for {self.group_size} people, for the {self.date} at {self.start_time}.'
