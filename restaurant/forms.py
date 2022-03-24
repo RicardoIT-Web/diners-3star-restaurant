5ae2c1b7c141b1db88725595e4388da777877a92
RicardoIT-Web <rick_piedade@hotmail.com>
2022-03-24 20:29:11 +0000

    Add date picker code to forms to allow user to use a calender

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -2,15 +2,22 @@ from .models import Booking
 from django import forms
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ('table', 'group_size', 'date', 'start_time', 'end_time', 'comment', )
+        widgets = {
+            'date': DateInput(),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['date'].widgets = forms.widgets.DateInput(
-            attrs={'type': 'date'})
+            attrs={'type': ''})
         self.fields['start_time'].widget = forms.widgets.TimeInput(
              attrs={'type': 'time'})
         self.fields['end_time'].widget = forms.widgets.TimeInput(
