2fec96e73d6e9345190db17c8f6406b7f1085412
RicardoIT-Web <rick_piedade@hotmail.com>
2023-01-22 21:24:52 +0000

    add railway.app as allowed host in settings.py

--- main/settings.py
+++ main/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['diners-3star-restaurant.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['diners-3star-restaurant.herokuapp.com', 'localhost', 'diners-3star-restaurant-production.up.railway.app']
 
 # # Add Render.com URL to allowed hosts
 # RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
